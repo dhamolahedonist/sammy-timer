8b8650fd5b703a6522f3963a8fce8c27fb6e2bf5
Samuel Oduba <samueloduba123@gmail.com>
2022-01-15 07:17:32 -0800

    updated timer

--- app.js
+++ app.js
@@ -7,26 +7,21 @@ function handleDecrease() {
     if (count > 0) {
        count = count - 1;
        updateCountElement();
-    } else {
-        alert('Ha our count is already zero na')
     }
-
 }
 
 function handleReset() {
-    count = count = 0;
-    updateCountElement();
-    
+    if(count != 0) {
+         count = 0;
+        updateCountElement();
+    }
 }
 
 function handleIncrease() {
     if (count < 20) {
-    count = count + 1;
-     updateCountElement();
-    }else {
-        alert('maximum number exceeded')
-    }
-        
+        count = count + 1;
+        updateCountElement();
+     }           
 }
 
 function updateCountElement() {
